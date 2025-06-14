fddbf66ed651cd183e8af75e20f9fc13c573fc4e
Lautaro Intraub <lauti.intraub@gmail.com>
2025-06-14 19:07:19 -0300

    Validaciones añadidas

--- router/libroRouter.js
+++ router/libroRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import LibroController from "../controllers/libroController.js";
-import validatePost from "../validations/libroValidator.js";
+import {validatePost, validatePut, validateDelete} from "../validations/libroValidator.js";
 
 const libroController = new LibroController();
 const libroRouter = Router();
@@ -8,9 +8,7 @@ const libroRouter = Router();
 libroRouter.get("/", libroController.getAllLibroController);
 libroRouter.get("/:id", libroController.getLibroByIdController);
 libroRouter.post("/", validatePost, libroController.createLibroController);
-libroRouter.put("/:id", libroController.updateLibroController);
-libroRouter.delete("/:id", libroController.deleteLibroController);
-
-
+libroRouter.put("/:id", validatePut, libroController.updateLibroController);
+libroRouter.delete("/:id", validateDelete, libroController.deleteLibroController);
 
 export default libroRouter;
