1cacf437a4c11fea2dc247f4fbef7830efcfa932
Lautaro Intraub <lauti.intraub@gmail.com>
2025-06-14 19:07:57 -0300

    Validaciones creadas

--- validations/libroValidator.js
+++ validations/libroValidator.js
@@ -1,4 +1,3 @@
-
 const validatePost = (req, res, next) => {
   const { titulo, autor, editorial, año, genero } = req.body;
   if (
@@ -13,6 +12,35 @@ const validatePost = (req, res, next) => {
     return res.status(400).json({
       mensaje:
         "El título, autor y editorial son obligatorios. El año debe ser un número entero entre 1450 y el año actual",
+    });
+  }
+  next();
+};
+
+const validatePut = (req, res, next) => {
+  const { titulo, autor, editorial, año, genero } = req.body;
+  if (
+    !titulo ||
+    !autor ||
+    !editorial ||
+    titulo.trim() === "" ||
+    autor.trim() === "" ||
+    editorial.trim() === "" ||
+    (genero && genero.trim() === "")
+  ) {
+    return res.status(400).json({
+      mensaje:
+        "No se puede actualizar el libro ya que alguno de los campos obligatorios está vacío o no es válido. El año debe ser un número entero entre 1450 y el año actual",
+    });
+  }
+  next();
+};
+
+validateDelete = (req, res, next) => {
+  const { id } = req.params;
+  if (!id || isNaN(id)) {
+    return res.status(400).json({
+      mensaje: "El ID del libro es obligatorio y debe ser un número entero",
     });
   }
   next();
