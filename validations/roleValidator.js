c10915e8f074c6b2c684266fa1659cc145e4d30e
Lautaro Intraub <lauti.intraub@gmail.com>
2025-06-17 10:14:07 -0300

    Corrección en la comparación de id en el delete

--- validations/roleValidator.js
+++ validations/roleValidator.js
@@ -1,5 +1,3 @@
-import { validate } from "uuid";
-
 const validatePost = (req, res, next) => {
   const { roleName } = req.body;
 
@@ -27,9 +25,9 @@ const validatePut = (req, res, next) => {
 const validateDelete = (req, res, next) => {
   const { id } = req.params;
 
-  if (!id || !validate(id)) {
+  if (!id || isNaN(id)) {
     return res.status(400).json({
-      mensaje: "El ID del rol es obligatorio y debe ser un UUID válido.",
+      mensaje: "El ID del rol es obligatorio y debe ser un número válido.",
     });
   }
 
