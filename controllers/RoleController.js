56774c5e437fb0d77b81f328221418982debcabd
Lautaro Intraub <lauti.intraub@gmail.com>
2025-06-16 21:51:22 -0300

    Creación del controller de rol

---
+++ controllers/RoleController.js
@@ -0,0 +1,104 @@
+import RoleService from "../services/RoleService.js";
+
+class RoleController {
+  roleService = new RoleService();
+
+  getAllRolesController = async (req, res) => {
+    try {
+      const roles = await this.roleService.getAllRolesService();
+      res.status(200).send({
+        success: true,
+        message: roles,
+      });
+    } catch (error) {
+      res.status(400).send({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+
+  getRoleByIdController = async (req, res) => {
+    try {
+      const role = await this.roleService.getRoleServiceById(req.params.id);
+      if (role === null) {
+        return res.status(404).send({
+          success: false,
+          message: "Rol no encontrado",
+        });
+      }
+      res.status(200).send({
+        success: true,
+        message: role,
+      });
+    } catch (error) {
+      res.status(400).send({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+
+  createRoleController = async (req, res) => {
+    try {
+      const { roleName } = req.body;
+      const role = await this.roleService.createRoleService({ roleName });
+      res.status(201).send({
+        success: true,
+        message: role,
+      });
+    } catch (error) {
+      res.status(400).send({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+
+  updateRoleController = async (req, res) => {
+    try {
+      const { roleName } = req.body;
+      const role = await this.roleService.updateRoleService(req.params.id, {
+        roleName,
+      });
+      if (role === null) {
+        return res.status(404).send({
+          success: false,
+          message: "Rol no encontrado",
+        });
+      }
+      res.status(200).send({
+        success: true,
+        message: role,
+      });
+    } catch (error) {
+      res.status(400).send({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+
+  deleteRoleController = async (req, res) => {
+    try {
+      const role = await this.roleService.deleteRoleService(req.params.id);
+      if (role === null) {
+        return res.status(404).send({
+          success: false,
+          message: "Rol no encontrado",
+        });
+      }
+      res.status(200).send({
+        success: true,
+        message: "Rol eliminado correctamente",
+      });
+    } catch (error) {
+      res.status(400).send({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+}
+
+export default RoleController;
